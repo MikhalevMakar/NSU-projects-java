bf6686a15b0ed59592cac48a058ad9a8bebb77f3
Makar <110274589+MikhalevMakar@users.noreply.github.com>
2023-02-26 14:35:51 +0700

    Update CheckerDouble.java
    deleter commment

--- Task2/src/main/java/org/ru/nsu/mikhalev/task2/CheckerDouble/CheckerDouble.java
+++ Task2/src/main/java/org/ru/nsu/mikhalev/task2/CheckerDouble/CheckerDouble.java
@@ -5,7 +5,7 @@ public  class CheckerDouble {
     public static boolean IsNumberFormat(String s) {
         try {
             Double.parseDouble(s);
-//            NumberUtils.isCreatable(s);
+            NumberUtils.isCreatable(s);
             Double.isInfinite(Double.parseDouble(s));
             Double.isFinite(Double.parseDouble(s));
             Double.isNaN(Double.parseDouble(s));
