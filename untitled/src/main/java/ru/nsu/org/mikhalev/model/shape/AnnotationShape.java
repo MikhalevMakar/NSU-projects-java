cfa1911ea3a2d6845ffe7de06f832d104faf200a
Makar <110274589+MikhalevMakar@users.noreply.github.com>
2023-03-23 13:57:13 +0700

    Delete untitled directory
    delete untiled

--- untitled/src/main/java/ru/nsu/org/mikhalev/model/shape/AnnotationShape.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.org.mikhalev.model.shape;
-
-import java.lang.annotation.*;
-@Target(value = {ElementType.TYPE})
-@Retention(value = RetentionPolicy.RUNTIME)
-@Inherited
-public  @interface AnnotationShape {}
-
-
